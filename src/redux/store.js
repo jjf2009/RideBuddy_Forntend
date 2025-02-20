02d3b0a26b8c475e1bb0556e1f59a7fcc762ba29
vvm-hash <vedvishnumarathe@gmail.com>
2025-02-20 10:30:27 +0530

    Added feature request slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit'
 import ridesApi from "./features/rides/ridesApi"
+import requestSlice from "./features/request/requestSlice" // Import the new slice
 
 export const store = configureStore({
   reducer: {
     [ridesApi.reducerPath]: ridesApi.reducer, // ✅ Add API reducer properly
+    requests: requestSlice // Add request slice to Redux store
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(ridesApi.middleware), // ✅ Only add API middleware
-});
+    getDefaultMiddleware().concat(ridesApi.middleware) // ✅ Only add API middleware
+})
